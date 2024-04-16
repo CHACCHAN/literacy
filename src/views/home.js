9ebfeffdd28e14ad866ad4bb7d3040501f4ae081
CHACCHAN <chacchan.info@gmail.com>
2024-04-17 00:02:53 +0900

    フォントの追加

--- src/views/home.js
+++ src/views/home.js
@@ -1,7 +1,7 @@
 const home = {
     template:
     `
-        <h1 class="text-primary">aa</h1>
+        <h1 class="text-primary zen-kurenaido-regular">これはペンです。紛れもない事実です。国際連盟</h1>
         <p class="btn btn-primary" @click="console.log('aa')">aaa</p>
     `
 }
