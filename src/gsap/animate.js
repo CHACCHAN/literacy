5aa8af1c89af53a9ee6131bbd86e12a360364de1
CHACCHAN <chacchan.info@gmail.com>
2024-04-21 01:59:38 +0900

    とりあえず

--- src/gsap/animate.js
+++ src/gsap/animate.js
@@ -1,23 +1,44 @@
-gsap.fromTo(".fadeAnimateElm", { 
-        opacity: 0,
-        y: 100,
-        rotate: 30,
-        scale: 0,
+export const myAnimations = {
+    home: () => {
+        setTimeout(() => {
+            gsap.from('#rotateIcon2', {
+                duration: 1,
+                rotate: -30,
+                scale: 0,
+            });
+        }, 30);
     },
-    { 
-        opacity: 1,
-        y: 0,
-        rotate: 0,
-        scale: 1,
-        duration: 1
-});
+    about: () => {
+        setTimeout(() => {
+            gsap.from('#rotateIcon', {
+                duration: 2,
+                rotate: -100,
+                x: 200,
+                opacity: 0,
+                onComplete: () => {
+                    gsap.fromTo('#rotateIcon', 
+                        {
+                            y: 0,
+                        },
+                        {
+                            y: 10,
+                            ease: 'power1.inOut',
+                            stagger: {
+                                each: 5,
+                                repeat: -1,
+                                yoyo: true
+                            }
+                        });
+                }
+            });
 
-gsap.fromTo(".fadeAnimateMoveUp", {
-        opacity: 0,
-        y: 100,
-    },
-    {
-        opacity: 1,
-        y: 0,
-        duration: 1.5
-});
+            gsap.from('#fadeIn', {
+                duration: 1,
+                y: Math.floor(Math.random() * 200),
+                opacity: 0,
+                ease: 'none',
+                stagger: .5,
+            });
+        }, 30);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
-const { createApp, ref, onMounted } = Vue;
+import { myAnimations } from './gsap/animate.js';
+
+const { createApp, ref, watch, onMounted, onUpdated } = Vue;
 const { createRouter, createWebHistory } = VueRouter;
 
 export const fetchTemplate = async (pathName) => {
     const response = await fetch(location.origin + location.pathname + 'src/views/' + pathName);
     return await response.text();
+}
+
+const progressBar = () => {
+    var bar = new ProgressBar.Line(container, {
+        strokeWidth: 4,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: {width: '100%', height: '100%'},
+        from: {color: '#FFEA82'},
+        to: {color: '#ED6A5A'},
+        step: (state, circle) => {
+            circle.path.setAttribute('stroke', state.color);
+        }
+    });
+
+    bar.animate(1.0);
 }
 
 const router = createRouter({
@@ -12,26 +33,61 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
+            call: 'ホーム',
             component: { template: await fetchTemplate('home.html') }
         },
         {
             path: '/about',
             name: 'about',
+            call: '私について',
             component: { template: await fetchTemplate('about.html') }
         },
         {
             path: '/photo',
             name: 'photo',
+            call: '写真',
             component: { template: 'asadaaa'}
         },
     ]
 });
 
+router.beforeEach((to, from, next) => {
+    const tl = gsap.timeline();
+    tl.to('.wrapper', {
+        duration: 1.4,
+        opacity: 0.8,
+        onStart: () => {
+            document.getElementById('progressBar').innerHTML = '<div id="container" class="position-fixed w-100" style="height: 5px;top: -11px;"></div>';
+            progressBar();
+        },
+        onComplete: () => {
+            next();
+            document.getElementById('container').remove();
+        }
+    }).to(".wrapper", {
+        duration: 0.5,
+        opacity: 1
+    }, 1);
+});
+
 const app = createApp({
     setup() {
         onMounted(() => {
-
+            getRouting();
         });
+
+        watch(() => router.currentRoute.value.path, () => {
+            getRouting();
+        });
+
+        const getRouting = () => {
+            Object.values(myAnimations).forEach(fn => fn());
+            for(let i = 0; i < router.options.routes.length; i++) {
+                if(router.options.routes[i].path == location.pathname.replace('/literacy', '')) {
+                    document.title = router.options.routes[i].call;
+                }
+            }
+        }
 
         return {
             data: 'test'
