f2fd0ef4edff6da5f0d6d75c1a793cf13d2865e3
CHACCHAN <chacchan.info@gmail.com>
2024-04-20 22:11:00 +0900

    おもしろくなった

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
 const { createApp, ref, onMounted } = Vue;
 const { createRouter, createWebHistory } = VueRouter;
 
+export const fetchTemplate = async (pathName) => {
+    const response = await fetch(location.origin + location.pathname + 'src/views/' + pathName);
+    return await response.text();
+}
+
 const router = createRouter({
-    history: createWebHistory('/literacy/'),
+    history: createWebHistory(location.pathname),
     routes: [
         {
             path: '/',
             name: 'home',
-            component: home
+            component: { template: await fetchTemplate('home.html') }
         },
         {
             path: '/about',
             name: 'about',
-            component: about
+            component: { template: await fetchTemplate('about.html') }
         },
         {
             path: '/photo',
--- src/views/home.js
+++
@@ -1,6 +0,0 @@
-const home = {
-    template:
-    `
-        まだなにもないよ
-    `
-}
