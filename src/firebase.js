9aa82a2384a52edd5f321efa2fabb3962f87ba21
CHACCHAN <chacchan.info@gmail.com>
2024-04-27 00:11:06 +0900

    チャット機能の一部

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebas
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js';
 import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
 
 class firebase {
     constructor() {
@@ -13,6 +14,7 @@ class firebase {
             messagingSenderId: "364095826489",
             appId: "1:364095826489:web:b20b75b553d1753e94bbe8"
         });
+        this.database = getDatabase();
         this.provider = new GoogleAuthProvider();
         this.provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
         this.auth = getAuth();
@@ -38,6 +40,10 @@ class firebase {
         await signOut(this.auth)
         .then(() => setTimeout(() => window.location.reload(), 100))
         .catch((error) => console.log(error));
+    }
+
+    async db_write(path, obj) {
+        await set(ref(this.database, path), obj);
     }
 }
 
--- src/views/chat.js
+++ src/views/chat.js
@@ -2,4 +2,52 @@ import firebase from "../firebase.js";
 
 const fb = new firebase();
 
-console.log(fb);
+window.clickEvent = () => {
+
+}
+
+window.inputTextArea = (e) => {
+    const PADDING_Y = 20;
+    const textarea = e.target;
+    const lines = (textarea.value + '\n').match(/\n/g).length;
+
+    let lineHeight = getComputedStyle(textarea).lineHeight;
+
+    lineHeight = lineHeight.replace(/[^-\d\.]/g, '');
+    textarea.style.height = lineHeight * lines + PADDING_Y + 'px';
+}
+
+window.db_store_newThread = (e, title, detail, error) => {
+    const titleElm = document.getElementById(title);
+    const detailElm = document.getElementById(detail);
+    const errorElm = document.getElementById(error);
+    const buttonElm = e.target;
+
+    if(!titleElm.value.trim() || !detailElm.value.trim()) {
+        errorElm.classList.remove('d-none');
+    } else { 
+        errorElm.classList.add('d-none');
+
+        const buttonElmContent = buttonElm.innerHTML;
+        roadButton(buttonElm, buttonElmContent, true);
+        fb.db_write('threads/', {
+            title: titleElm.value,
+            detail: detailElm.value,
+        })
+        .then((result) => roadButton(buttonElm, buttonElmContent, false))
+        .catch((error) => {
+            alert(error);
+            roadButton(buttonElm, buttonElmContent, false);
+        });
+    }
+}
+
+function roadButton(elm, defaultElm, flag) {
+    if(flag) {
+        elm.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>   処理中';
+        elm.disabled = true;
+    } else {
+        elm.innerHTML = defaultElm;
+        elm.disabled = false;
+    }
+}
